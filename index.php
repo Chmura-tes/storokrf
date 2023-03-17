bc85455e991a79ba00bc852ed1743123db05a899
aaaaaaaaaaaaaaaa <plaszczkawodna@gmail.com>
2023-03-17 12:49:47 +0100

    ::D

--- index.php
+++ index.php
@@ -40,7 +40,10 @@
                 $opis = $d['opis'];
 				$obrazek = $d['link-obrazka'];
 				?>
-				<li style="background-image: url('<?php echo $obrazek ?>');">
+				<li style="
+				background-image:
+				linear-gradient(to left, rgba(255, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 59%, rgba(0, 0, 0, 0.65) 100%),
+				url('<?php echo $obrazek ?>');">
 						<div class="tbox">
 							<h2><?php echo $id_gry ?></h2>
 							<h3><?php echo $nazwa ?></h3>
@@ -59,25 +62,35 @@
 			<div class="right">
 				<form id="form" method="POST">
 					<label>nazwa gry</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
+					<input type="text" name="nazwagry" required> <br>
+
 					<label>opis</label>
-					<input type="text" placeholder="opis" name="opis" required>
+					<input type="text" name="opis" required> <br>
+
 					<label>wydawca</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
-					<label>nazwa gry</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
-					<label>nazwa gry</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
-					<label>nazwa gry</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
-					<label>nazwa gry</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
-					<label>nazwa gry</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
-					<label>nazwa gry</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
-					<label>nazwa gry</label>
-					<input type="text" placeholder="nazwa gry" name="nazwagry" required>
+					<input type="text" name="nazwawydawcy" required> <br>
+
+					<label>producent</label>
+					<input type="text" name="nazwaproducenta" required> <br>
+
+					<label>obrazek</label>
+					<input type="image" name="linkobrazka" required> <br>
+
+					<label>link do strony</label>
+					<input type="text" name="linkstrony"> <br>
+
+					<label>multiplayer</label>
+					<input type="checkbox" name="mp" required> <br>
+
+					<label>singleplayer</label>
+					<input type="checkbox" name="sp" required> <br>
+
+					<label>dostępność na steamie</label>
+					<input type="checkbox" name="steam" required> <br>
+
+					<label>data wydania gry</label>
+					<input type="date" name="data" required> <br>
+
 					<button type="submit">Submit</button>
 	</form>
 			</div>
@@ -92,6 +105,9 @@
 		</div>
 
 	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script type="text/javascript" src="ajax-script.js"></script>
 </body>
 
 </html>
